0546fb52d7d6706ee6a98bd64787b47ac3c19b6a
Eunpyeong Hong <62169778+chananababa@users.noreply.github.com>
2022-09-14 19:30:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
     const source = core.getInput('source')
     const find = core.getInput('find')
     const replace = core.getInput('replace')
-    const branchName = source.replace(find, replace)
+    const branchName = source.replaceAll(find, replace)
     core.setOutput('value', branchName)
 } catch (error) {
     core.setFailed(error.message)
