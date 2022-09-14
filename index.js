ece8bc3351a439fe13ca056d091aa9b20c834cec
Eunpyeong Hong <62169778+chananababa@users.noreply.github.com>
2022-09-14 19:43:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
+function replaceAll(str, searchStr, replaceStr) {
+  return str.split(searchStr).join(replaceStr);
+}
+
 try {
     const source = core.getInput('source')
     const find = core.getInput('find')
     const replace = core.getInput('replace')
-    const branchName = source.replaceAll(find, replace)
+    const branchName = replaceAll(source, find, replace)
     core.setOutput('value', branchName)
 } catch (error) {
     core.setFailed(error.message)
